db21e0637f12351ee60d69c139e9e225745f8c77
Sean Yang <seany071305@gmail.com>
2024-09-20 21:41:15 -0400

    deleted extra tabs and turned tabs into spaces

--- rtl/DataMemory.sv
+++ rtl/DataMemory.sv
@@ -17,25 +17,25 @@ Note: right now, only word addressable is implemented, FUTURE: have byteEnable f
 */
 
 module DataMemory #(
-		parameter ADDR_WIDTH = 64, //how many bits wide are memory addresses?
-		parameter DATA_WIDTH = 64, //how many bits wide is the data bus?
-		
-		parameter WORD_BYTES_2POW = 3, //how many bytes are in a word? (2^x)
-		parameter WORD_BYTES = 1 << WORD_BYTES_2POW, //number of bytes in a word (converted from WORD_BYTES_2POW)
-		
-		/* verilator lint_off UNUSEDPARAM */ // might be used later
-		parameter WORD_WIDTH = WORD_BYTES * 8, //number of bits in a word = no of bytes * 8 bits in a byt
-		
-		parameter DEPTH_2POW = 12, //how many words are stored in the memory block? (2^x)
-		parameter DEPTH = 1 << DEPTH_2POW //number of words stored (converted from DEPTH_2POW)
+   parameter ADDR_WIDTH = 64, //how many bits wide are memory addresses?
+   parameter DATA_WIDTH = 64, //how many bits wide is the data bus?
+   
+   parameter WORD_BYTES_2POW = 3, //how many bytes are in a word? (2^x)
+   parameter WORD_BYTES = 1 << WORD_BYTES_2POW, //number of bytes in a word (converted from WORD_BYTES_2POW)
+   
+   /* verilator lint_off UNUSEDPARAM */ // might be used later
+   parameter WORD_WIDTH = WORD_BYTES * 8, //number of bits in a word = no of bytes * 8 bits in a byt
+   
+   parameter DEPTH_2POW = 12, //how many words are stored in the memory block? (2^x)
+   parameter DEPTH = 1 << DEPTH_2POW //number of words stored (converted from DEPTH_2POW)
 )
 ( 
-		input logic [ADDR_WIDTH-1:0] address_in, //memory address input
-		input logic [DATA_WIDTH-1:0] data_in, //data (to write) input
-		input logic writeEnable_in, //enable write
-		input logic readEnable_in, //enable read
-		input logic clk_in, //clock signal
-		output logic [DATA_WIDTH-1:0] data_out //data output (read)
+   input logic [ADDR_WIDTH-1:0] address_in, //memory address input
+   input logic [DATA_WIDTH-1:0] data_in, //data (to write) input
+   input logic writeEnable_in, //enable write
+   input logic readEnable_in, //enable read
+   input logic clk_in, //clock signal
+   output logic [DATA_WIDTH-1:0] data_out //data output (read)
 );
 
 // intialize ram block
@@ -47,30 +47,30 @@ logic [DEPTH_2POW-1:0] wordNumber; // which word line to access (taken by dividi
 // calculate word line number from the memory address
 always_comb begin
 
-	/* verilator lint_off WIDTHTRUNC */ // to implement: check if the calculated wordNumber exceeds the max >DEPTH-1
-	wordNumber = address_in >> WORD_BYTES_2POW; //divide the byte address by the number of bytes in a word to get the wordline nubmer
+   /* verilator lint_off WIDTHTRUNC */ // to implement: check if the calculated wordNumber exceeds the max >DEPTH-1
+   wordNumber = address_in >> WORD_BYTES_2POW; //divide the byte address by the number of bytes in a word to get the wordline nubmer
 
 end
 
 // asynchronous reading
 always_comb begin
 
-	/* verilator lint_off UNUSEDPARAM */ // to do: reevaluate necessity of readEnable
-	if(readEnable_in) begin
-		data_out = ram[wordNumber]; // reads the entire word (word addressable) -> shold be byte addressable in the future
-	end
-	
+   /* verilator lint_off UNUSEDPARAM */ // to do: reevaluate necessity of readEnable
+   if(readEnable_in) begin
+      data_out = ram[wordNumber]; // reads the entire word (word addressable) -> shold be byte addressable in the future
+   end
+   
 end
 
 // synchronous writing
 always_ff@(posedge clk_in) begin
-	
-	if(writeEnable_in) begin
-		ram[wordNumber] <= data_in; //writes the entire word to the address -> should be byte addressable in the future (see example below)
-	end
-	
+   
+   if(writeEnable_in) begin
+      ram[wordNumber] <= data_in; //writes the entire word to the address -> should be byte addressable in the future (see example below)
+   end
+   
 end
-	
+   
 endmodule 
 
 // BRAM template from Intel for reference
@@ -82,35 +82,35 @@ endmodule
 // and with a control for writing single bytes into the memory word; byte enable
 
 module byte_enabled_simple_dual_port_ram
-	#(parameter int
-		ADDR_WIDTH = 6,
-		BYTE_WIDTH = 8,
-		BYTES = 4,
-			WIDTH = BYTES * BYTE_WIDTH
+   #(parameter int
+      ADDR_WIDTH = 6,
+      BYTE_WIDTH = 8,
+      BYTES = 4,
+         WIDTH = BYTES * BYTE_WIDTH
 )
 ( 
-	input [ADDR_WIDTH-1:0] waddr,
-	input [ADDR_WIDTH-1:0] raddr,
-	input [BYTES-1:0] be,
-	input [BYTE_WIDTH-1:0] wdata, 
-	input we, clk,
-	output reg [WIDTH - 1:0] q
+   input [ADDR_WIDTH-1:0] waddr,
+   input [ADDR_WIDTH-1:0] raddr,
+   input [BYTES-1:0] be,
+   input [BYTE_WIDTH-1:0] wdata, 
+   input we, clk,
+   output reg [WIDTH - 1:0] q
 );
-	localparam int WORDS = 1 << ADDR_WIDTH ;
+   localparam int WORDS = 1 << ADDR_WIDTH ;
 
-	// use a multi-dimensional packed array to model individual bytes within the word
-	logic [BYTES-1:0][BYTE_WIDTH-1:0] ram[0:WORDS-1];
+   // use a multi-dimensional packed array to model individual bytes within the word
+   logic [BYTES-1:0][BYTE_WIDTH-1:0] ram[0:WORDS-1];
 
-	always_ff@(posedge clk)
-	begin
-		if(we) begin
-		// edit this code if using other than four bytes per word
-			if(be[0]) ram[waddr][0] <= wdata;
-			if(be[1]) ram[waddr][1] <= wdata;
-			if(be[2]) ram[waddr][2] <= wdata;
-			if(be[3]) ram[waddr][3] <= wdata;
-	end
-		q <= ram[raddr];
-	end
+   always_ff@(posedge clk)
+   begin
+      if(we) begin
+      // edit this code if using other than four bytes per word
+         if(be[0]) ram[waddr][0] <= wdata;
+         if(be[1]) ram[waddr][1] <= wdata;
+         if(be[2]) ram[waddr][2] <= wdata;
+         if(be[3]) ram[waddr][3] <= wdata;
+   end
+      q <= ram[raddr];
+   end
 endmodule : byte_enabled_simple_dual_port_ram
 */
