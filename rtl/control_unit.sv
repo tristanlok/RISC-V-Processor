d5b1d5f4df44c98fd84899e743eb7a7a08802344
tristanlok <tristan.lok@uwaterloo.ca>
2024-09-23 11:18:16 -0400

    Framework for Control Unit

---
+++ rtl/control_unit.sv
@@ -0,0 +1,15 @@
+module control_unit (
+	input [31:0]	instruction,
+	
+	output			reg_alu_mux,
+	output			mem_read,
+	output			mem_write,
+	output [1:0]	alu_op,
+	output			reg_write,
+	output			data_reg_mux,
+	output			branch_ctrl
+);
+
+assign 
+
+endmodule
