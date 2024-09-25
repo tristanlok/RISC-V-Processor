e9fb73d19a61bbf913561f108c3b6447d5748c6f
tristanlok <tristan.lok@uwaterloo.ca>
2024-09-25 15:51:42 -0400

    Completed Program Counter

---
+++ rtl/program_counter.sv
@@ -0,0 +1,12 @@
+module program_counter (
+	input logic		[63:0] instruction_i,
+	input logic				 clock,
+	
+	output logic	[63:0] instruction_o
+);
+
+always_ff @(posedge clock) begin
+	instruction_o <= instruction_i;
+end
+
+endmodule
