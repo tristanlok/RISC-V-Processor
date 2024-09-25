f536602cff01eb5aa0ea937ea5ff78f3b79958a5
Sean <99679916+ShurnYurnYang@users.noreply.github.com>
2024-09-25 17:56:35 -0400

    added wordNumber intermediate

--- rtl/instruction_mem.sv
+++ rtl/instruction_mem.sv
@@ -16,8 +16,16 @@ module instruction_mem #(
 // Create the instruction memory
 logic [WORD_SIZE - 1:0][7:0] ram [0:MEM_DEPTH - 1]; // Creates MEM_DEPTH number of 32bit rows in memory
 
+logic [MEM_DEPTH-1:0] wordNumber;
+
+// calculating word number by dividing mem address by 4
 always_comb begin
-	data_o = ram[mem_addr >> WORD_SIZE_POW];
+	wordNumber = [mem_addr >> WORD_SIZE_POW];
+end
+
+// read instruction	
+always_comb begin
+	data_o = ram[wordNumber];
 end
 	
 endmodule
