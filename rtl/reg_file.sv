6dd96620a9d868c58f0b334ee0520d6758792bd5
tristanlok <tristan.lok@uwaterloo.ca>
2024-10-01 10:36:03 -0400

    changed net names

--- rtl/reg_file.sv
+++ rtl/reg_file.sv
@@ -1,24 +1,23 @@
 module reg_file (
-   input [4:0]    reg_num_1, reg_num_2, // indicate that its input with _in | this is the register number we are reading from
-   input [4:0]    write_reg, // this is the register number that we're writing to
-   input [63:0]   write_data,
-   input          clk, //indicate _in
+   input [4:0]    rs1_in, rs2_in,                  // register numbers to read data from
+   input [4:0]    write_reg_num_in,                // register number to write data to
+   input [63:0]   reg_write,                       // data to be written into register
+   input          clk_in,
    input          write_en,
 
-   output [63:0]  reg_data_1, reg_data_2 //indicate _out
+   output [63:0]  reg_data1_out, reg_data2_out     // data output of the two specified registers
 );
 
 reg [63:0] registers [31:0];     // 32 64-bit general registers
 
 // Combinational Logic for decoding register number into register data
 
-//change to always_comb
 assign reg_data_1 = registers[reg_num_1];
 assign reg_data_2 = registers[reg_num_2];
 
 // Sequential Logic for writing data into the target register
 
-always @(posedge clk) begin      // this always can change when I learn more about always
+always_ff @(posedge clk) begin
    if (write_en) begin
       registers[write_reg] <= write_data;
    end
