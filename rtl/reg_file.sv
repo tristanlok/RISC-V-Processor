563040d746dee29ba6ea498a6f03e2c0f08bd700
tristanlok <tristan.lok@uwaterloo.ca>
2024-10-01 10:38:46 -0400

    fixed all bugs

--- rtl/reg_file.sv
+++ rtl/reg_file.sv
@@ -1,7 +1,7 @@
 module reg_file (
    input [4:0]    rs1_in, rs2_in,                  // register numbers to read data from
-   input [4:0]    write_reg_num_in,                // register number to write data to
-   input [63:0]   reg_write,                       // data to be written into register
+   input [4:0]    rd_in,                           // register number to write data to
+   input [63:0]   data_write,                      // data to be written into register
    input          clk_in,
    input          write_en,
 
@@ -12,14 +12,14 @@ reg [63:0] registers [31:0];     // 32 64-bit general registers
 
 // Combinational Logic for decoding register number into register data
 
-assign reg_data_1 = registers[reg_num_1];
-assign reg_data_2 = registers[reg_num_2];
+assign reg_data1_out = registers[rs1_in];
+assign reg_data2_out = registers[rs2_in];
 
 // Sequential Logic for writing data into the target register
 
-always_ff @(posedge clk) begin
+always_ff @(posedge clk_in) begin
    if (write_en) begin
-      registers[write_reg] <= write_data;
+      registers[rd_in] <= data_write;
    end
 end
 
