6b0a61fe600986276fd04617fa142465387d9e4e
Sean Yang <seany071305@gmail.com>
2024-10-01 17:19:17 -0400

    added opcode, funct3, and funct7 as inputs and changed variable names as needed

--- rtl/ControlUnit/ControlUnit.sv
+++ rtl/ControlUnit/ControlUnit.sv
@@ -1,8 +1,7 @@
 module ControlUnit import temp::*; ( // 1'b0 = OFF 1'b1 = ON
-   /* verilator lint_off UNUSEDSIGNAL */
-   input logic [31:0]            instruction,
-   /* verilator lint_on UNUSEDSIGNAL */
-   
+   input logic [6:0] opcode_in,
+	input logic [2:0] funct3_in,
+	input logic [6:0] funct7_in,
    output reg_alu_ctrl_t      reg_alu_mux,
    output logic               mem_read,
    output logic               mem_write,
@@ -12,20 +11,8 @@ module ControlUnit import temp::*; ( // 1'b0 = OFF 1'b1 = ON
    output logic               branch_ctrl
 );
 
-// intermediates
-logic [6:0] opcode;
-logic [2:0] funct3;
-logic [6:0] funct7;
-
-// extract sections from instruction
 always_comb begin
-   opcode = instruction[6:0];
-   funct3 = instruction[14:12];
-   funct7 = instruction[31:25];
-end   
-
-always_comb begin
-   case(opcode)
+   case(opcode_in)
       7'b0000011: begin //I-TYPE
          // Currently the only instruction supported in I-TYPE is LD
          reg_alu_mux    = IMM_MUX;
@@ -50,10 +37,10 @@ always_comb begin
       
       7'b0110011: begin //R-TYPE
          // Currently supports 4 instructions (ADD, SUB, AND, & OR)
-         case (funct3) 
+         case (funct3_in) 
             3'b000: begin
                // Current supports 2 instructions (ADD & SUB)
-               case (funct7)
+               case (funct7_in)
                   7'b0000000: begin
                      // ADD Instruction
                      reg_alu_mux    = REG_MUX;
