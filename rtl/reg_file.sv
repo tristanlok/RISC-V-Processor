46467458ab004a5f5c4fd093675ef123dd8a660e
tristanlok <tristan.lok@uwaterloo.ca>
2024-10-01 10:58:15 -0400

    fixed spacing

--- rtl/reg_file.sv
+++ rtl/reg_file.sv
@@ -1,17 +1,17 @@
 module reg_file #(
-	parameter	REG_DATA_WIDTH_POW = 6,
-	localparam	REG_DATA_WIDTH = 1 << REG_DATA_WIDTH_POW,
-	
-	parameter	REG_MEM_DEPTH_POW = 5,
-	localparam	REG_MEM_DEPTH = 1 << REG_MEM_DEPTH_POW
+   parameter   REG_DATA_WIDTH_POW = 6,
+   localparam  REG_DATA_WIDTH = 1 << REG_DATA_WIDTH_POW,
+   
+   parameter   REG_MEM_DEPTH_POW = 5,
+   localparam  REG_MEM_DEPTH = 1 << REG_MEM_DEPTH_POW
 )(
    input [REG_MEM_DEPTH_POW-1:0]    rs1_in, rs2_in,                  // register numbers to read data from
    input [REG_MEM_DEPTH_POW-1:0]    rd_in,                           // register number to write data to
-   input [REG_DATA_WIDTH-1:0]   		data_write,                      // data to be written into register
-   input          						clk_in,
-   input          						write_en,
+   input [REG_DATA_WIDTH-1:0]       data_write,                      // data to be written into register
+   input                            clk_in,
+   input                            write_en,
 
-   output [REG_DATA_WIDTH-1:0]  		reg_data1_out, reg_data2_out     // data output of the two specified registers
+   output [REG_DATA_WIDTH-1:0]      reg_data1_out, reg_data2_out     // data output of the two specified registers
 );
 
 reg [REG_DATA_WIDTH-1:0] registers [REG_MEM_DEPTH-1:0];     // 32 64-bit general registers
