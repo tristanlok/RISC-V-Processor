8cb9f7d6fbdad6b23ede1e7d6b26b1b0cc35d6f5
tristanlok <tristan.lok@uwaterloo.ca>
2024-10-09 13:53:14 -0400

    Added Sync Reset

--- rtl/ProgramCounter/ProgramCounter.sv
+++ rtl/ProgramCounter/ProgramCounter.sv
@@ -1,12 +1,17 @@
 module ProgramCounter (
    input logic    [63:0] instruction_i,
    input logic           clock,
+   input logic           reset,
    
    output logic   [63:0] instruction_o
 );
 
 always_ff @(posedge clock) begin
-   instruction_o <= instruction_i;
+   if (reset) begin
+      instruction_o <= '0;
+   end else begin
+      instruction_o <= instruction_i;
+   end
 end
 
 endmodule
