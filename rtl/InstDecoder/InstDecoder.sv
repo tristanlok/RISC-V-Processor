0280146d5f37143d9da64a793f832823e5c6d14b
tristanlok <tristan.lok@uwaterloo.ca>
2024-10-10 16:24:56 -0400

    Fixed formatting

--- rtl/InstDecoder/InstDecoder.sv
+++ rtl/InstDecoder/InstDecoder.sv
@@ -14,86 +14,87 @@ Note: currently only supports necesary instruction types (I, S, R, B)
 */
 
 module InstDecoder(
-   input logic [31:0] inst_in,
-   output logic [6:0] opcode,
-   output logic [4:0] rs1,
-   output logic [4:0] rs2,
-   output logic [4:0] rd,
-   output logic [11:0] imm,
-   output logic [2:0] funct3,
-   output logic [6:0] funct7
+   input    logic [31:0]   inst_in,
+   
+   output   logic [6:0]    opcode,
+   output   logic [4:0]    rs1,
+   output   logic [4:0]    rs2,
+   output   logic [4:0]    rd,
+   output   logic [11:0]   imm,
+   output   logic [2:0]    funct3,
+   output   logic [6:0]    funct7
 );
 
-// extract opcode from instruction
-always_comb begin
+   // extract opcode from instruction
+   always_comb begin
 
-   opcode = inst_in[6:0];
+      opcode = inst_in[6:0];
 
-end   
+   end   
 
-// extract format agnostic information
-always_comb begin
-   
-   /* verilator lint_off CASEINCOMPLETE */
-   // switch type based on opcode
-   unique case(inst_in[6:0])
-   
-      7'b0000011: begin //I-TYPE
-         rd = inst_in[11:7];
-         funct3 = inst_in[14:12];
-         rs1 = inst_in[19:15];
-         imm = inst_in[31:20];
-			
-			rs2 = 5'bx; // Dont Care
-			funct7 = 7'bx; // Dont Care
-      end
+   // extract format agnostic information
+   always_comb begin
       
-      7'b0100011: begin //S-TYPE
-         imm[4:0] = inst_in[11:7];
-         funct3 = inst_in[14:12];
-         rs1 = inst_in[19:15];
-         rs2 = inst_in[24:20];
-         imm[11:5] = inst_in[31:25];
-			
-			rd = 5'bx; // Dont Care
-			funct7 = 7'bx; // Dont Care
-      end
+      /* verilator lint_off CASEINCOMPLETE */
+      // switch type based on opcode
+      unique case(inst_in[6:0])
       
-      7'b0110011: begin //R-TYPE
-         rd = inst_in[11:7];
-         funct3 = inst_in[14:12];
-         rs1 = inst_in[19:15];
-         rs2 = inst_in[24:20];
-         funct7 = inst_in[31:25];
-			
-			imm = 12'bx; // DOnt Care
-      end
-      
-      7'b1100011: begin //B-TYPE //NOTE: imm for B-type is technically 13-bits because its left shift 
-                                 //by 1 but that will be handled outside of instruction decoder
-         imm[10] = inst_in[7];
-         imm[3:0] = inst_in[11:8];
-         funct3 = inst_in[14:12];
-         rs1 = inst_in[19:15];
-         rs2 = inst_in[24:20];
-         imm[9:4] = inst_in[30:25];
-         imm[11] = inst_in[31];
-			
-			rd = 5'bx; // Dont Care
-			funct7 = 7'bx; // Dont Care
-      end
-		
-		default : begin //Default - set everything to dont care
-			rs1 = 5'bx;
-			rs2 = 5'bx;
-			rd = 5'bx;
-			imm = 12'bx;
-			funct3 = 3'bx;
-			funct7 = 7'bx;
-		end
-      
-   endcase
+         7'b0000011: begin //I-TYPE
+            rd = inst_in[11:7];
+            funct3 = inst_in[14:12];
+            rs1 = inst_in[19:15];
+            imm = inst_in[31:20];
+            
+            rs2 = 5'bx; // Dont Care
+            funct7 = 7'bx; // Dont Care
+         end
+         
+         7'b0100011: begin //S-TYPE
+            imm[4:0] = inst_in[11:7];
+            funct3 = inst_in[14:12];
+            rs1 = inst_in[19:15];
+            rs2 = inst_in[24:20];
+            imm[11:5] = inst_in[31:25];
+            
+            rd = 5'bx; // Dont Care
+            funct7 = 7'bx; // Dont Care
+         end
+         
+         7'b0110011: begin //R-TYPE
+            rd = inst_in[11:7];
+            funct3 = inst_in[14:12];
+            rs1 = inst_in[19:15];
+            rs2 = inst_in[24:20];
+            funct7 = inst_in[31:25];
+            
+            imm = 12'bx; // DOnt Care
+         end
+         
+         7'b1100011: begin //B-TYPE //NOTE: imm for B-type is technically 13-bits because its left shift 
+                                    //by 1 but that will be handled outside of instruction decoder
+            imm[10] = inst_in[7];
+            imm[3:0] = inst_in[11:8];
+            funct3 = inst_in[14:12];
+            rs1 = inst_in[19:15];
+            rs2 = inst_in[24:20];
+            imm[9:4] = inst_in[30:25];
+            imm[11] = inst_in[31];
+            
+            rd = 5'bx; // Dont Care
+            funct7 = 7'bx; // Dont Care
+         end
+         
+         default : begin //Default - set everything to dont care
+            rs1 = 5'bx;
+            rs2 = 5'bx;
+            rd = 5'bx;
+            imm = 12'bx;
+            funct3 = 3'bx;
+            funct7 = 7'bx;
+         end
+         
+      endcase
 
-end
-   
+   end
+      
 endmodule
--- tb/InstDecoder/InstDecoder_assertions.sv
+++ tb/InstDecoder/InstDecoder_assertions.sv
@@ -1,56 +1,58 @@
 module InstDecoder_assertions(
-    input logic [31:0] inst_in,
-    output logic [4:0] rs1,
-    output logic [4:0] rs2,
-    output logic [4:0] rd,
-    output logic [11:0] imm,
-    output logic [2:0] funct3,
-    output logic [6:0] funct7
+   input    logic [31:0]   inst_in,
+   
+   output   logic [6:0]    opcode,
+   output   logic [4:0]    rs1,
+   output   logic [4:0]    rs2,
+   output   logic [4:0]    rd,
+   output   logic [11:0]   imm,
+   output   logic [2:0]    funct3,
+   output   logic [6:0]    funct7
 );
 
-// Instance of the DUT (Device Under Test)
-InstDecoder DUT (.inst_in, .rs1, .rs2, .rd, .imm, .funct3, .funct7);
+   // Instance of the DUT (Device Under Test)
+   InstDecoder DUT (.inst_in, .rs1, .rs2, .rd, .imm, .funct3, .funct7);
 
-// Assertions for each type of instruction
-always_comb begin
-  unique case (inst_in[6:0])
-		7'b0000011: begin // I-TYPE
-			 assert(rd == inst_in[11:7]);
-			 assert(funct3 == inst_in[14:12]);
-			 assert(rs1 == inst_in[19:15]);
-			 assert(imm == inst_in[31:20]);
-		end
+   // Assertions for each type of instruction
+   always_comb begin
+      unique case (inst_in[6:0])
+         7'b0000011: begin // I-TYPE
+             assert(rd == inst_in[11:7]);
+             assert(funct3 == inst_in[14:12]);
+             assert(rs1 == inst_in[19:15]);
+             assert(imm == inst_in[31:20]);
+         end
 
-		7'b0100011: begin // S-TYPE
-			 assert(rs1 == inst_in[19:15]);
-			 assert(rs2 == inst_in[24:20]);
-			 assert(funct3 == inst_in[14:12]);
-			 assert(imm[4:0] == inst_in[11:7]);
-			 assert(imm[11:5] == inst_in[31:25]);
-		end
+         7'b0100011: begin // S-TYPE
+             assert(rs1 == inst_in[19:15]);
+             assert(rs2 == inst_in[24:20]);
+             assert(funct3 == inst_in[14:12]);
+             assert(imm[4:0] == inst_in[11:7]);
+             assert(imm[11:5] == inst_in[31:25]);
+         end
 
-		7'b0110011: begin // R-TYPE
-			 assert(rd == inst_in[11:7]);
-			 assert(rs1 == inst_in[19:15]);
-			 assert(rs2 == inst_in[24:20]);
-			 assert(funct3 == inst_in[14:12]);
-			 assert(funct7 == inst_in[31:25]);
-		end
+         7'b0110011: begin // R-TYPE
+             assert(rd == inst_in[11:7]);
+             assert(rs1 == inst_in[19:15]);
+             assert(rs2 == inst_in[24:20]);
+             assert(funct3 == inst_in[14:12]);
+             assert(funct7 == inst_in[31:25]);
+         end
 
-		7'b1100011: begin // B-TYPE
-			 assert(rs1 == inst_in[19:15]);
-			 assert(rs2 == inst_in[24:20]);
-			 assert(funct3 == inst_in[14:12]);
-			 assert(imm[3:0] == inst_in[11:8]);
-			 assert(imm[9:4] == inst_in[30:25]);
-			 assert(imm[10] == inst_in[7]);
-			 assert(imm[11] == inst_in[31]);
-		end
+         7'b1100011: begin // B-TYPE
+             assert(rs1 == inst_in[19:15]);
+             assert(rs2 == inst_in[24:20]);
+             assert(funct3 == inst_in[14:12]);
+             assert(imm[3:0] == inst_in[11:8]);
+             assert(imm[9:4] == inst_in[30:25]);
+             assert(imm[10] == inst_in[7]);
+             assert(imm[11] == inst_in[31]);
+         end
 
-		default: begin
-			 // No assertions for unsupported opcodes
-		end
-  endcase
-end
+         default: begin
+             // No assertions for unsupported opcodes
+         end
+     endcase
+   end
 
 endmodule
