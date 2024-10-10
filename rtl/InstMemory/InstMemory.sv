ba52ef3db7e0c2b7334d747b83b0eb2b53e28933
tristanlok <tristan.lok@uwaterloo.ca>
2024-10-10 16:20:23 -0400

    Fixed formatting

--- rtl/InstMemory/InstMemory.sv
+++ rtl/InstMemory/InstMemory.sv
@@ -14,22 +14,22 @@ module InstMemory #(
    output   logic [DATA_WIDTH-1:0]           data_out
 );
 
-// Create the instruction memory
-logic [WORD_SIZE - 1:0][7:0] ram [0:MEM_DEPTH - 1]; // Creates MEM_DEPTH number of 32bit rows in memory
+	// Create the instruction memory
+	logic [WORD_SIZE - 1:0][7:0] ram [0:MEM_DEPTH - 1]; // Creates MEM_DEPTH number of 32bit rows in memory
 
-logic [MEM_DEPTH-1:0] wordNumber;
+	logic [MEM_DEPTH-1:0] wordNumber;
 
-// On Initialization, Use $readmemh for hexadecimal format
-initial begin
-   $readmemh("", ram); // Load instructions from the file
-end
+	// On Initialization, Use $readmemh for hexadecimal format
+	initial begin
+		$readmemh("", ram); // Load instructions from the file
+	end
 
-// calculating word number by dividing mem address by 4
-always_comb begin
-   wordNumber = address_in >> WORD_SIZE_POW;
-end
+	// calculating word number by dividing mem address by 4
+	always_comb begin
+		wordNumber = address_in >> WORD_SIZE_POW;
+	end
 
-// Assigns the Instruction Data Out
-assign data_out = ram[wordNumber];
+	// Assigns the Instruction Data Out
+	assign data_out = ram[wordNumber];
    
 endmodule
