8e3993c1828b096efb725d6a25a38707f7216478
tristanlok <tristan.lok@uwaterloo.ca>
2024-10-14 12:58:50 -0400

    Changed output names for clearer output descriptions and general formatting changes

--- rtl/InstrDecoder/InstrDecoder.sv
+++ rtl/InstrDecoder/InstrDecoder.sv
@@ -16,75 +16,70 @@ Note: currently only supports necesary instruction types (I, S, R, B)
 module InstrDecoder(
    input    logic [31:0]   instr_in,
    
-   output   logic [6:0]    opcode,
-   output   logic [4:0]    rs1,
-   output   logic [4:0]    rs2,
-   output   logic [4:0]    rd,
-   output   logic [11:0]   imm,
-   output   logic [2:0]    funct3,
-   output   logic [6:0]    funct7
+   output   logic [6:0]    opcode_out,
+   output   logic [4:0]    rs1_out,
+   output   logic [4:0]    rs2_out,
+   output   logic [4:0]    rd_out,
+   output   logic [11:0]   imm_out,
+   output   logic [2:0]    funct3_out,
+   output   logic [6:0]    funct7_out
 );
 
    // extract opcode from instruction
    always_comb begin
-
-      opcode = instr_in[6:0];
-
+      opcode_out = instr_in[6:0];
    end   
 
    // extract format agnostic information
    always_comb begin
-      
-      /* verilator lint_off CASEINCOMPLETE */
+   
       // switch type based on opcode
       unique case(instr_in[6:0])
       
          7'b0000011: begin //I-TYPE
-            rd = instr_in[11:7];
-            funct3 = instr_in[14:12];
-            rs1 = instr_in[19:15];
-            imm = instr_in[31:20];
+            rd_out      = instr_in[11:7];
+            funct3_out  = instr_in[14:12];
+            rs1_out     = instr_in[19:15];
+            imm_out     = instr_in[31:20];
             
-            rs2 = '0; // Dont Care
-            funct7 = '0; // Dont Care
+            // Don't Care
+            rs2_out     = '0;
+            funct7_out  = '0;
          end
          
          7'b0100011: begin //S-TYPE
-            imm[4:0] = instr_in[11:7];
-            funct3 = instr_in[14:12];
-            rs1 = instr_in[19:15];
-            rs2 = instr_in[24:20];
-            imm[11:5] = instr_in[31:25];
+            funct3_out  = instr_in[14:12];
+            rs1_out     = instr_in[19:15];
+            rs2_out     = instr_in[24:20];
+            imm_out     = {instr_in[31:25], instr_in[11:7]};
             
-            rd = '0; // Dont Care
-            funct7 = '0; // Dont Care
+            // Don't Care
+            rd_out         = '0;
+            funct7_out     = '0;
          end
          
          7'b0110011: begin //R-TYPE
-            rd = instr_in[11:7];
-            funct3 = instr_in[14:12];
-            rs1 = instr_in[19:15];
-            rs2 = instr_in[24:20];
-            funct7 = instr_in[31:25];
+            rd_out      = instr_in[11:7];
+            funct3_out  = instr_in[14:12];
+            rs1_out     = instr_in[19:15];
+            rs2_out     = instr_in[24:20];
+            funct7_out  = instr_in[31:25];
             
-            imm = '0; // Dont Care
+            // Don't Care
+            imm_out     = '0;
          end
          
          7'b1100011: begin //B-TYPE //NOTE: imm for B-type is technically 13-bits because its left shift 
                                     //by 1 but that will be handled outside of instruction decoder
-            imm[10] = instr_in[7];
-            imm[3:0] = instr_in[11:8];
-            funct3 = instr_in[14:12];
-            rs1 = instr_in[19:15];
-            rs2 = instr_in[24:20];
-            imm[9:4] = instr_in[30:25];
-            imm[11] = instr_in[31];
+            funct3_out  = instr_in[14:12];
+            rs1_out     = instr_in[19:15];
+            rs2_out     = instr_in[24:20];
+            imm_out     = {instr_in[31], instr_in[7], instr_in[30:25], instr_in[11:8]};
             
-            rd = '0; // Dont Care
-            funct7 = '0; // Dont Care
+            // Don't Care
+            rd_out      = '0;
+            funct7_out  = '0;
          end         
       endcase
-
    end
-  
 endmodule
