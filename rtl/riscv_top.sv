e2b82fc6aea503e815b26a3f7775f31da2f79dc4
tristanlok <tristan.lok@uwaterloo.ca>
2024-10-15 00:27:32 -0400

    Added Sign Extension + ALU Src Mux

--- rtl/riscv_top.sv
+++ rtl/riscv_top.sv
@@ -11,6 +11,10 @@
     // INSTR ALSO CHANGES ALL NAMES
 );
    // Internal net instantiation
+	
+	// Misc.
+	logic [63:0]   imm_ext;
+	logic [63:0]	mux_alu_data;
    
    // Originates from programCounter
    logic [63:0]   pc_instrMem_addr;
@@ -23,13 +27,18 @@
    logic [4:0]    rs1;
    logic [4:0]    rs2;
    logic [4:0]    rd;
-   logic [11:0]   imm;
+   logic [11:0]   imm_12b;
    logic [2:0]    funct3;
    logic [6:0]    funct7;
    
    // Originates from controlUnit
-	
-	Alu_Src_t	
+	aluDataSrc_t		aluSrcCtrl;
+   logic          	memRead;
+   logic          	memWrite;
+   aluOperation_t		aluOp;
+   logic          	regWrite;
+   regDataSrc_t   	regSrcCtrl;
+   logic          	branchCtrl;
    
    // Originates from regFile
    logic [REG_DATA_WIDTH-1:0]    regFile_ALU_data1;
@@ -61,22 +70,25 @@
       .rs1_out(rs1),
       .rs2_out(rs2),
       .rd_out(rd),
-      .imm_out(imm),
+      .imm_out(imm_12b),
       .funct3_out(funct3),
       .funct7_out(funct7)
    );
 	
+	// Sign-extend IMM
+	assign imm_ext = {{(REG_DATA_WIDTH-12){imm_12b[11]}}, imm_12b};		// Replicates imm_12b's MSB [REG_DATA_WIDTH-12] times (currently 52), then concatenates it ahead of imm_12b
+	
 	ControlUnit controlUnit(
-      .opcode_in(),
-      .funct3_in(),
-      .funct7_in(),
-      .alu_src_mux_ctrl(),
-      .mem_read(),
-      .mem_write(),
-      .alu_op(),
-      .reg_write(),
-      .reg_src_mux_ctrl(),
-      .branch_ctrl()
+      .opcode_in(opcode),
+      .funct3_in(funct3),
+      .funct7_in(funct7),
+      .aluSrcCtrl_out(aluSrcCtrl),
+      .memRead_out(memRead),
+      .memWrite_out(memWrite),
+      .aluOp_out(aluOp),
+      .regWrite_out(regWrite),
+      .regSrcCtrl_out(regSrcCtrl),
+      .branchCtrl_out(branchCtrl)
    );
    
    RegFile regFile #(
@@ -84,17 +96,22 @@
    )(
       .clk_in(clk_in),
       .reset(),
-      .write_en(),
+      .regWrite_ctrl(),
       .rs1_in(rs1),
       .rs2_in(rs2),
       .rd_in(rd),
-      .write_data_in(write_data),
-      .reg_data1_out(regFile_ALU_data1),
-      .reg_data2_out(regFile_ALU_data2)
+      .writeData_in(write_data),
+      .regData1_out(regFile_ALU_data1),
+      .regData2_out(regFile_ALU_data2)
    );
    
-   unique case (/*expression*/)     // MUX to switch between IMM & Register 2 Data
-      
+	// MUX to switch between IMM & Register 2 Data
+	always_comb begin
+		unique case (aluSrcCtrl)
+			ALU_SRC_REG: mux_alu_data = regFile_ALU_data2;
+			ALU_SRC_IMM: mux_alu_data = imm_ext;
+		endcase;
+	end      
 
    ALU alu (
       .operand1_in(), 
