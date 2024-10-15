c01425268869b80be902e2c16a9983700c28a66c
tristanlok <tristan.lok@uwaterloo.ca>
2024-10-14 23:47:10 -0400

    Changed all types to match CamelCase Style

--- rtl/ControlSignals.sv
+++ rtl/ControlSignals.sv
@@ -5,17 +5,17 @@ package ControlSignals;
       OP_SUB = 3'b000,  // ALU operation: Subtract
       OP_AND = 3'b001,  // ALU operation: AND
       OP_OR  = 3'b011   // ALU operation: OR
-   } Alu_Operation_t;
+   } aluOperation_t;
 
    // Enum to control the source of the ALU operand (Register or Immediate)
    typedef enum logic {
       ALU_SRC_REG,  // ALU input source: Register
       ALU_SRC_IMM   // ALU input source: Immediate
-   } Alu_Src_t;
+   } aluDataSrc_t;
 
    // Enum to control the source of data for the register (Data from memory or ALU)
    typedef enum logic {
       REG_SRC_MEM,  // Register data source: Data from memory
       REG_SRC_ALU   // Register data source: Data from ALU
-   } Reg_Data_Src_t;
+   } regDataSrc_t;
 endpackage
