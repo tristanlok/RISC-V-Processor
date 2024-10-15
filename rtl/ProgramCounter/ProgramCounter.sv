8b495e54c50180cbe7235a5547864ce571936719
tristanlok <tristan.lok@uwaterloo.ca>
2024-10-15 02:07:09 -0400

    Added support for variable parameter address size Program Counter

--- rtl/ProgramCounter/ProgramCounter.sv
+++ rtl/ProgramCounter/ProgramCounter.sv
@@ -1,9 +1,12 @@
-module ProgramCounter (
-   input    logic          clk_in,
-   input    logic          reset,
-   input    logic [63:0]   instr_in,         // Next Instruction is recieved
+module ProgramCounter #(
+   parameter   ADDR_WIDTH_POW = 6,                      // Using Powers as Parameter ensures width is a power of 2
+   localparam  ADDR_WIDTH = 1 << ADDR_WIDTH_POW,
+)(
+   input    logic                    clk_in,
+   input    logic                    reset,
+   input    logic [ADDR_WIDTH-1:0]   instr_in,         // Next Instruction is recieved
    
-   output   logic [63:0]   instr_out         // Current Instruction is released
+   output   logic [ADDR_WIDTH-1:0]   instr_out         // Current Instruction is released
 );
 
    always_ff @(posedge clk_in) begin
