bf301bdc5992e329bf1d40c722e37a99483d1544
tristanlok <tristan.lok@uwaterloo.ca>
2024-10-15 03:51:09 -0400

    Updated ProgramCounter Formal Verification

--- tb/ProgramCounter/ProgramCounter_assertions.sv
+++ tb/ProgramCounter/ProgramCounter_assertions.sv
@@ -1,19 +1,24 @@
-module ProgramCounter_assertions(
-   input    logic [63:0]   instruction_i,
-   input    logic          clk_in,
+module ProgramCounter_assertions #(
+   parameter   ADDR_WIDTH_POW = 6,                      // Using Powers as Parameter ensures width is a power of 2
+   parameter   ADDR_WIDTH = 1 << ADDR_WIDTH_POW
+)(
+   input    logic                    clk_in,
+   input    logic                    reset,
+   input    logic [ADDR_WIDTH-1:0]   instr_in,         // Next Instruction is recieved
    
-   output   logic [63:0]   instruction_o
+   output   logic [ADDR_WIDTH-1:0]   instr_out         // Current Instruction is released
 );
 
-   ProgramCounter DUT (
-         .instruction_i(instruction_i), 
+   ProgramCounter #(.ADDR_WIDTH_POW(ADDR_WIDTH_POW)) DUT (
          .clk_in(clk_in), 
-         .instruction_o(instruction_o));
+         .reset(reset), 
+         .instr_in(instr_in),
+			.instr_out(instr_out));
 
    always_ff @(posedge clk_in) begin
       
       // Asserts that Current Instruction is equal to Past Next Instruction
-      assert (instruction_o == $past(instruction_i));
+      assert (instr_out == $past(instr_in));
       
    end
 
