6d7133beb79a72b5a534c7e6954600a1b54c9f75
tristanlok <tristan.lok@uwaterloo.ca>
2024-10-15 03:29:49 -0400

    Updated DataMemory Formal Verification

--- tb/DataMemory/DataMemory_assertions.sv
+++ tb/DataMemory/DataMemory_assertions.sv
@@ -1,48 +1,42 @@
 module DataMemory_assertions #(
-   parameter ADDR_WIDTH = 64, //how many bits wide are memory addresses?
-   parameter DATA_WIDTH = 64, //how many bits wide is the data bus?
+   parameter   DATA_WIDTH_POW = 6,                    // Using Powers as Parameter ensures width is a power of 2
+   parameter   DATA_WIDTH = 1 << DATA_WIDTH_POW,
    
-   parameter WORD_BYTES_2POW = 3, //how many bytes are in a word? (2^x)
-   parameter WORD_BYTES = 1 << WORD_BYTES_2POW, //number of bytes in a word (converted from WORD_BYTES_2POW)
+   parameter   ADDR_WIDTH_POW = 6,                    // Using Powers as Parameter ensures width is a power of 2
+   parameter   ADDR_WIDTH = 1 << ADDR_WIDTH_POW,
    
-   /* verilator lint_off UNUSEDPARAM */ // might be used later
-   parameter WORD_WIDTH = WORD_BYTES * 8, //number of bits in a word = no of bytes * 8 bits in a byt
+   parameter   MEM_DEPTH_POW = 6,                    //how many words are stored in the memory block? (2^x)          [SET 12 -> 6 FOR FASTER VERIFICATION]
+   parameter   MEM_DEPTH = 1 << MEM_DEPTH_POW,        //number of words stored (converted from MEM_MEM_DEPTH_POW)
    
-   parameter DEPTH_2POW = 6, //how many words are stored in the memory block? (2^x) [CHANGED 12 -> 6 FOR FORMAL VERIFICATION]
-   parameter DEPTH = 1 << DEPTH_2POW //number of words stored (converted from DEPTH_2POW)
-)
-( 
-   input logic [ADDR_WIDTH-1:0] address_in, //memory address input
-   input logic [DATA_WIDTH-1:0] data_in, //data (to write) input
-   input logic writeEnable_in, //enable write
-   input logic readEnable_in, //enable read
-   input logic clk_in, //clock signal
-   input logic reset,
+   parameter   WORD_BYTES_POW = 3,                    //how many bytes are in a word? (2^x)
+   parameter   WORD_BYTES = 1 << WORD_BYTES_POW       //number of bytes in a word (converted from WORD_BYTES_POW)
+)(
+   input    logic                   clk_in,           //clock signal
+   input    logic                   reset,            //reset
+   input    logic                   memWrite_ctrl,    //enable write
+   input    logic                   memRead_ctrl,     //enable read
+   input    logic [ADDR_WIDTH-1:0]  addr_in,          //memory address input
+   input    logic [DATA_WIDTH-1:0]  data_in,          //data (to write) input
    
-   
-   output logic [DATA_WIDTH-1:0] data_out //data output (read)
+   output   logic [DATA_WIDTH-1:0]  data_out          //data output (read)
 );
 
    // instantiating DUT
    DataMemory #( 
-      .ADDR_WIDTH(ADDR_WIDTH), 
-      .DATA_WIDTH(DATA_WIDTH), 
-      .WORD_BYTES_2POW(WORD_BYTES_2POW), 
-      .WORD_BYTES(WORD_BYTES), 
-      .WORD_WIDTH(WORD_WIDTH), 
-      .DEPTH_2POW(DEPTH_2POW), 
-      .DEPTH(DEPTH)
+      .DATA_WIDTH_POW(DATA_WIDTH_POW), 
+      .ADDR_WIDTH_POW(ADDR_WIDTH_POW), 
+      .MEM_DEPTH_POW(MEM_DEPTH_POW)
    ) DUT (
-      .address_in(address_in), 
+      .addr_in(addr_in), 
       .data_in(data_in), 
-      .writeEnable_in(writeEnable_in), 
-      .readEnable_in(readEnable_in), 
+      .memWrite_ctrl(memWrite_ctrl), 
+      .memRead_ctrl(memRead_ctrl), 
       .clk_in(clk_in),
       .reset(reset),
       .data_out(data_out)
    );
 
-   logic [ADDR_WIDTH-1:0] address_in_delay; // delayed memory address input
+   logic [ADDR_WIDTH-1:0] addr_in_delay; // delayed memory address input
    logic [DATA_WIDTH-1:0] data_in_delay; // delayed write data input
    logic [DATA_WIDTH-1:0] data_out_delay; // delayed read data output
    
@@ -53,7 +47,7 @@ module DataMemory_assertions #(
    // initializing delays to 0
    initial begin
    
-      address_in_delay = '0;
+      addr_in_delay = '0;
       data_in_delay = '0;
       data_out_delay = '0;
       was_written = '0;
@@ -64,21 +58,21 @@ module DataMemory_assertions #(
    // assumptions
    always_ff @(posedge clk_in) begin
    
-      assume(address_in < (DEPTH * 8)); // constrain size of address to maximum allowed by depth
-      assume(writeEnable_in == 1'b0 || writeEnable_in == 1'b1); // ensure writeEnable_in is either 0 or 1
-      assume(readEnable_in == 1'b1); // assume read is always on
+      assume(addr_in < (MEM_DEPTH * 8)); // constrain size of address to maximum allowed by depth
+      assume(memWrite_ctrl == 1'b0 || memWrite_ctrl == 1'b1); // ensure memWrite_ctrl is either 0 or 1
+      assume(memRead_ctrl == 1'b1); // assume read is always on
       
-      assume(reset + writeEnable_in < 2);
+      assume(reset + memWrite_ctrl < 2);
       
    end
    
    // saving delayed values
    always_ff @(posedge clk_in) begin
       
-      address_in_delay <= address_in;
+      addr_in_delay <= addr_in;
       data_out_delay <= data_out;
       
-      if (writeEnable_in) begin
+      if (memWrite_ctrl) begin
          data_in_delay <= data_in;
          was_written <= 1'b1;
       end else begin
@@ -95,14 +89,14 @@ module DataMemory_assertions #(
    
    // read after write integrity check
    always_ff @(posedge clk_in) begin
-      if (!was_reset && was_written && (address_in_delay == address_in)) begin
+      if (!was_reset && was_written && (addr_in_delay == addr_in)) begin
          assert (data_out == data_in_delay);
       end
    end
    
-   // writeEnable_in low will not write
+   // memWrite_ctrl low will not write
    always_ff @(posedge clk_in) begin
-      if (!was_reset && !was_written && (address_in_delay == address_in)) begin
+      if (!was_reset && !was_written && (addr_in_delay == addr_in)) begin
          assert (data_out_delay == data_out);
       end
    end
