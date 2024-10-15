15edf87b7d11c29bbaea54ebfb46d6a1d1522cf8
tristanlok <tristan.lok@uwaterloo.ca>
2024-10-15 02:05:11 -0400

    Updated i/o names and parameters Also commented out Formal Verification code

--- rtl/DataMemory/DataMemory.sv
+++ rtl/DataMemory/DataMemory.sv
@@ -17,76 +17,73 @@ Note: right now, only word addressable is implemented, FUTURE: have byteEnable f
 */
 
 module DataMemory #(
-   parameter ADDR_WIDTH = 64, //how many bits wide are memory addresses?
-   parameter DATA_WIDTH = 64, //how many bits wide is the data bus?
+   parameter   DATA_WIDTH_POW = 6,                    // Using Powers as Parameter ensures width is a power of 2
+   localparam  DATA_WIDTH = 1 << DATA_WIDTH_POW,
    
-   parameter WORD_BYTES_2POW = 3, //how many bytes are in a word? (2^x)
-   parameter WORD_BYTES = 1 << WORD_BYTES_2POW, //number of bytes in a word (converted from WORD_BYTES_2POW)
+   parameter   ADDR_WIDTH_POW = 6,                    // Using Powers as Parameter ensures width is a power of 2
+   localparam  ADDR_WIDTH = 1 << ADDR_WIDTH_POW,
    
-   /* verilator lint_off UNUSEDPARAM */ // might be used later
-   parameter WORD_WIDTH = WORD_BYTES * 8, //number of bits in a word = no of bytes * 8 bits in a byt
+   parameter   MEM_DEPTH_POW = 12,                    //how many words are stored in the memory block? (2^x)
+   localparam  MEM_DEPTH = 1 << MEM_DEPTH_POW,        //number of words stored (converted from MEM_MEM_DEPTH_POW)
    
-   parameter DEPTH_2POW = 12, //how many words are stored in the memory block? (2^x)
-   parameter DEPTH = 1 << DEPTH_2POW //number of words stored (converted from DEPTH_2POW)
-)
-( 
-   input logic [ADDR_WIDTH-1:0] address_in, //memory address input
-   input logic [DATA_WIDTH-1:0] data_in, //data (to write) input
-   input logic writeEnable_in, //enable write
-   input logic readEnable_in, //enable read
-   input logic clk_in, //clock signal
-   input logic reset, //clock signal   
+   localparam  WORD_BYTES_POW = 3,                    //how many bytes are in a word? (2^x)
+   localparam  WORD_BYTES = 1 << WORD_BYTES_POW       //number of bytes in a word (converted from WORD_BYTES_POW)
+)(
+   input    logic                   clk_in,           //clock signal
+   input    logic                   reset,            //reset
+   input    logic                   memWrite_ctrl,    //enable write
+   input    logic                   memRead_ctrl,     //enable read
+   input    logic [ADDR_WIDTH-1:0]  addr_in,          //memory address input
+   input    logic [DATA_WIDTH-1:0]  data_in,          //data (to write) input
    
-   output logic [DATA_WIDTH-1:0] data_out //data output (read)
+   output   logic [DATA_WIDTH-1:0]  data_out          //data output (read)
 );
 
-   // intialize ram block
-   logic [WORD_BYTES-1:0][7:0] ram [0:DEPTH-1]; //[number of bytes in a word][number of bits in a byte] ram [number of words (starting at index 0)]
-
-   // internal signals
-   logic [DEPTH_2POW-1:0] wordNumber; // which word line to access (taken by dividing memory address by the number of bytes in a word
-
-   // initialize all memory location sto 0
+   // SIMULATION ONLY: Initialize all memory location sto 0
+   /*
    initial begin
-      for (int i = 0; i < DEPTH; i = i + 1) begin
+      for (int i = 0; i < MEM_DEPTH; i = i + 1) begin
          ram[i] = '0;  // Initialize each register to all 0's
       end
    end
+   */
+
+   // intialize ram block
+   logic [WORD_BYTES-1:0][7:0] ram [0:MEM_DEPTH-1]; //[number of bytes in a word][number of bits in a byte] ram [number of words (starting at index 0)]
+
+   // internal signals
+   logic [MEM_DEPTH_POW-1:0] wordNumber; // which word line to access (taken by dividing memory address by the number of bytes in a word
 
    // calculate word line number from the memory address
    always_comb begin
-
-      /* verilator lint_off WIDTHTRUNC */ // to implement: check if the calculated wordNumber exceeds the max >DEPTH-1
-      wordNumber = address_in >> WORD_BYTES_2POW; //divide the byte address by the number of bytes in a word to get the wordline nubmer
-
+      /* verilator lint_off WIDTHTRUNC */ // to implement: check if the calculated wordNumber exceeds the max >MEM_DEPTH-1
+      wordNumber = addr_in >> WORD_BYTES_POW; //divide the byte address by the number of bytes in a word to get the wordline nubmer
    end
 
    // asynchronous reading
    always_comb begin
-
-      /* verilator lint_off UNUSEDPARAM */ // to do: reevaluate necessity of readEnable | expecing readEnable = 1
-      if(readEnable_in) begin
+      /* verilator lint_off UNUSEDPARAM */ // to do: reevaluate necessity of memRead_ctrl | expecing memRead_ctrl = 1
+      if(memRead_ctrl) begin
          data_out = ram[wordNumber]; // reads the entire word (word addressable) -> shold be byte addressable in the future
       end else begin
-         data_out = '0; // output 0s if readEnable is not on
+         data_out = '0; // output 0s if memRead_ctrl is not on
       end
-      
    end
 
    // synchronous writing
    always_ff @(posedge clk_in) begin
-
       if (reset) begin
-         // ram <= '{default: '0};
+         ram <= '{default: '0};
          
-         for (int i = 0; i < DEPTH; i = i + 1) begin
+         // ONLY USED FOR FORMAL VERIFICATION TO SET REGISTERS AS '0 (casting not supported by symbiyosys)
+         /*
+         for (int i = 0; i < MEM_DEPTH; i = i + 1) begin
             ram[i] <= '0; // Initialize each register to all 0's
          end
-      
-      end else if(writeEnable_in) begin
+         */
+      end else if(memWrite_ctrl) begin
          ram[wordNumber] <= data_in; //writes the entire word to the address -> should be byte addressable in the future (see example below)
-      end
-         
+      end 
    end
    
 endmodule 
